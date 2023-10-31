bc20db417d8faec98994c29816d4614d15c670b7
Nabin  Kharal <nabin@brainants.com>
2023-10-31 16:10:03 +0545

    flutter and rn working together

--- android/app/src/main/java/com/example/flutter_merger/RNActivity.java
+++ android/app/src/main/java/com/example/flutter_merger/RNActivity.java
@@ -1,47 +1,58 @@
 package com.example.flutter_merger;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-
-import com.facebook.react.ReactActivity;
-import com.facebook.react.ReactActivityDelegate;
-import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
-import com.facebook.react.defaults.DefaultReactActivityDelegate;
-import com.google.android.material.snackbar.Snackbar;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
+import com.facebook.react.PackageList;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.common.LifecycleState;
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.facebook.soloader.SoLoader;
+import java.util.ArrayList;
+import java.util.List;
+import kotlin.jvm.internal.Intrinsics;
+import org.jetbrains.annotations.Nullable;
 
-import androidx.core.view.WindowCompat;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
-import com.example.flutter_merger.databinding.ActivityRnactivity2Binding;
+public final class RNActivity extends Activity implements DefaultHardwareBackBtnHandler {
+    private ReactRootView reactRootView;
+    private ReactInstanceManager reactInstanceManager;
 
-public class RNActivity extends ReactActivity {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        SoLoader.init((Context)this, false);
+        this.reactRootView = new ReactRootView((Context)this);
+        ArrayList var10000 = (new PackageList(this.getApplication())).getPackages();
+        Intrinsics.checkNotNullExpressionValue(var10000, "PackageList(application).packages");
+        List packages = (List)var10000;
+        ReactInstanceManager var10001 = ReactInstanceManager.builder().setApplication(this.getApplication()).setCurrentActivity((Activity)this).setBundleAssetName("index.android.bundle").setJSMainModulePath("index").addPackages(packages).setUseDeveloperSupport(BuildConfig.DEBUG).setInitialLifecycleState(LifecycleState.RESUMED).build();
+        Intrinsics.checkNotNullExpressionValue(var10001, "ReactInstanceManager.bui…MED)\n            .build()");
+        this.reactInstanceManager = var10001;
+        ReactRootView var3 = this.reactRootView;
+        if (var3 == null) {
+            Intrinsics.throwUninitializedPropertyAccessException("reactRootView");
+        }
 
-    /**
-     * Returns the name of the main component registered from JavaScript. This is used to schedule
-     * rendering of the component.
-     */
-    @Override
-    protected String getMainComponentName() {
-        return "flutter_merger";
+        if (var3 != null) {
+            var10001 = this.reactInstanceManager;
+            if (var10001 == null) {
+                Intrinsics.throwUninitializedPropertyAccessException("reactInstanceManager");
+            }
+
+            var3.startReactApplication(var10001, "react-native-merger", (Bundle)null);
+        }
+
+        ReactRootView var4 = this.reactRootView;
+        if (var4 == null) {
+            Intrinsics.throwUninitializedPropertyAccessException("reactRootView");
+        }
+
+        this.setContentView((View)var4);
     }
 
-    /**
-     * Returns the instance of the {@link ReactActivityDelegate}. Here we use a util class {@link
-     * DefaultReactActivityDelegate} which allows you to easily enable Fabric and Concurrent React
-     * (aka React 18) with two boolean flags.
-     */
-    @Override
-    protected ReactActivityDelegate createReactActivityDelegate() {
-        return new DefaultReactActivityDelegate(
-                this,
-                getMainComponentName(),
-                // If you opted-in for the New Architecture, we enable the Fabric Renderer.
-                DefaultNewArchitectureEntryPoint.getFabricEnabled());
+    public void invokeDefaultOnBackPressed() {
+        super.onBackPressed();
     }
 }
